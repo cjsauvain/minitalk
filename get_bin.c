d3fa4ff9b943663641353b245d2cd75e0c56bf80
Jordan Sauvain <jsauvain@3F3.42angouleme.fr>
2022-07-07 09:04:48 +0200

    Only first sentence prints well

--- get_bin.c
+++ get_bin.c
@@ -6,7 +6,7 @@
 /*   By: jsauvain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 15:05:45 by jsauvain          #+#    #+#             */
-/*   Updated: 2022/07/06 16:50:08 by jsauvain         ###   ########.fr       */
+/*   Updated: 2022/07/07 09:01:38 by jsauvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	power(int nb, int power)
 	int	n;
 
 	n = nb;
-	while (power)
+	if (power == 0)
+		return (1);
+	while (power > 1)
 	{
 		nb *= n;
 		power--;
@@ -28,20 +30,20 @@ int	power(int nb, int power)
 void	get_bin(int sig)
 {
 	static int	i = 0;
-	static int	nb = 0;
+	static int	nb = 6;
 
 	if (sig == SIGUSR1)
-		i += power(2, nb % 7);
-	nb++;
-	if (nb > 7 && i)
+		i += power(2, nb);
+	nb--;
+	if (nb < 0 && i)
 	{
-		ft_printf("%d", i);
+		ft_printf("%c", i);
 		i = 0;
-		nb = 0;
+		nb = 6;
 	}
-	else if (i == 0 && nb > 7)
+	else if (nb < 0 && !i)
 	{
 		ft_printf("\n");
-		nb = 0;
+		nb = 6;
 	}
 }
--- send_binaries.c
+++ send_binaries.c
@@ -6,7 +6,7 @@
 /*   By: jsauvain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 14:28:41 by jsauvain          #+#    #+#             */
-/*   Updated: 2022/07/06 16:47:45 by jsauvain         ###   ########.fr       */
+/*   Updated: 2022/07/07 08:57:55 by jsauvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,15 @@ void	send_binaries(pid_t pid, char *c)
 				kill(pid, SIGUSR2);
 			else if ((*c >> i & 1) == 1)
 				kill(pid, SIGUSR1);
-			usleep(500);
+			usleep(50);
 			i--;
 		}
 		c++;
+	}
+	while (i <= 6)
+	{
+		kill(pid, SIGUSR2);
+		usleep(50);
+		i++;
 	}
 }
